09eaac0a8f884cf01f55b0afc9a6b474df0157ab
mihikatyagi <151062167+mihikatyagi@users.noreply.github.com>
2025-12-11 21:17:24 +0530

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main style={{ padding: 20 }}>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default App
 
